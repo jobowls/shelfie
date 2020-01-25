70c0fb8c31efa86e1ea61401ce3b169757841b8f
Josh Bowler <joshbowler46@gmail.com>
2020-01-24 17:09:26 -0700

    update

--- server/ctrl.js
+++ server/ctrl.js
@@ -2,7 +2,7 @@ module.exports = {
 	getProduct: (req, res) => {
 		const db = req.app.get("db");
 		const {id} = req.params
-		db.get_meme(+id).then(response => {
+		db.get_product(+id).then(response => {
 			const data = response[0];
 			console.log(data)
 			res.status(200).send(data);
